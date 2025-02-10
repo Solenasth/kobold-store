17cb3b6217a9f343042e22ccd64ff8ff1912b2f4
Carlos Alcala <solenasthellion@gmail.com>
2025-02-10 14:09:21 -0600

    chore: :label: add product type

---
+++ src/components/types/Product.ts
@@ -0,0 +1,11 @@
+export type Product = {
+    id: number;
+    name: string;
+    type: string;
+    rarity: string;
+    tags: string[];
+    description: string;
+    price: number;
+    imageUrl: string;
+    imgAlt: string
+  };
